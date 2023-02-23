bd40d81cf0885106483d8da5d762eaae329da624
nachan28 <torajiro282828@gmail.com>
2023-02-23 17:02:19 +0900

    追加: いくつかのcssファイルを追加

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,21 @@
 import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
 const name = "Nanako Muramatsu";
 
 function Layout({ children }) {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" src="favicon.ico" />
       </Head>
-      <header>
-        <img src="/images/profile.png" alt="profile image"/>
+      <header className={styles.header}>
+        <img src="/images/profile.png" alt="profile image" className={utilStyles.borderCircle}/>
         <h1>{name}</h1>
       </header>
       <main>{children}</main>
-    </>
+    </div>
   );
 }
 
